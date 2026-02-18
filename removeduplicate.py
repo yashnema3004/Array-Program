7a656c3882aa7078ed4bc480a7359ffe7bc5befd
Yash Nema <yashnema30.04@gmail.com>
2026-02-18 13:14:02 +0530

    Create removeduplicate.py

---
+++ removeduplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        k = 1
+
+        for i in range(1,len(nums)):
+
+            if nums[i] != nums[k-1]:
+                nums[k] = nums[i]
+                k +=1
+
+        return k
+
+        
